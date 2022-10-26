8ddbdfa04b847201bb83e40cc511783fbbe19a70
LeTegu <107449459+LeTegu@users.noreply.github.com>
2022-10-26 14:20:58 -0500

    Deleted Webcam-Capture and added SleeveDetection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/glowCode/DetectingSignalSleeve.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/glowCode/DetectingSignalSleeve.java
@@ -1,44 +1,62 @@
-package org.firstinspires.ftc.teamcode.glowCode;
+/*ackage org.firstinspires.ftc.teamcode.glowCode;
 
-import com.vuforia.Image;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.videoio.VideoCapture;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import java.io.File;
-
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.stage.Stage;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.videoio.VideoCapture;
-import org.openftc.easyopencv.OpenCvWebcam;
 
-public class DetectingSignalSleeve extends HardwareMapping {
+//this is all shamelessly stealing from https://www.codespeedy.com/how-to-capture-image-in-java-using-opencv-with-webcam/
 
-    public int[][][] calculateData()
-    {
-        //I am going to shamelessly steal from vuforia
+public class DetectingSignalSleeve {
 
-        final int ROWS = 1280;
-        final int COLUMNS = 720;
-        int[][][] picture = new int[ROWS][COLUMNS][3];
+    Mat matrix = null;
+    public void start(Stage stage) throws FileNotFoundException, IOException {
+        update o = new update();
+        WritableImage Imgw = o.capureSnapShot();
+        o.saveImage();
+        ImageView img = new ImageView(Imgw);
+        img.setFitHeight(400);
+        img.setFitWidth(600);
+        img.setPreserveRatio(true);
+        Group r = new Group(img);
+        Scene s = new Scene(r, 600, 400);
+        stage.setTitle("Capturing an image");
+        stage.setScene(s);
+        stage.show();
+    }
+    public WritableImage capureSnapShot() {
+        WritableImage WritableImage = null;
+        System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+        VideoCapture c = new VideoCapture(0);
+        Mat matrix = new Mat();
+        c.read(matrix);
+        if( c.isOpened()) {
+            if (c.read(matrix)) {
+                BufferedImage image = new BufferedImage(matrix.width(),
+                        matrix.height(), BufferedImage.TYPE_3BYTE_BGR);
 
-        for (int i = 0; i < ROWS; i++)
-        {
-            for (int j = 0; j < COLUMNS; j++) {
-                //This should detect the Red in Red
-
-                //This should detect the Green
-
-                //This should detect the Blue
-
+                WritableRaster raster = image.getRaster();
+                DataBufferByte dataBuffer = (DataBufferByte) raster.getDataBuffer();
+                byte[] d = dataBuffer.getData();
+                matrix.get(0, 0, d);
+                this.matrix = matrix;
+                WritableImage = SwingFXUtils.toFXImage(image, null);
             }
         }
-        return picture;
+        return WritableImage;
     }
-}
+}*/
