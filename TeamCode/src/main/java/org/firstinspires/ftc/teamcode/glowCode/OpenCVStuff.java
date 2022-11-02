a5a402f311fd56624f270db0440e2119087e5386
LeTegu <107449459+LeTegu@users.noreply.github.com>
2022-11-01 19:17:06 -0500

    11/2/2023
    ITS FROM EL FUTURO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/glowCode/OpenCVStuff.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/glowCode/OpenCVStuff.java
@@ -2,9 +2,15 @@ package org.firstinspires.ftc.teamcode.glowCode;
 
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
+import java.util.Arrays;
+
 
 //this will be used later
 public class OpenCVStuff {
-    Mat image1 = Imgcodecs.imread("![](../../../../../../../../../Images/IMG_2810.jpg)");
+    Mat image1 = Imgcodecs.imread("![](../../../../../../../../../FtcRobotController/src/main/assets/IMG_2811.jpg)");
+    Mat image2 = Imgcodecs.imread("![](../../../../../../../../../FtcRobotController/src/main/assets/IMG_2810.jpg)");
 
+    while () {
+
+    }
 }
